eab0813efa8160f2af7bbf37ff79dca6c243e8de
jheemay23 <97150039+jheemay23@users.noreply.github.com>
2022-02-09 20:43:51 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//main.js
+$(function(){
+
+    //1단계 메뉴에 마우스가 올라가면 depth2가 보임
+    ///...마우스가 나가면 depth2 class on 제거
+    $('#gnb > ul > li').hover(
+        function(){
+            $(this).children().addClass('on');
+        },
+        function(){
+            $(this).children().removeClass('on')
+        }
+    );
+})
+
+//패럴랙스 배경 스크롤
+$.stellar({
+    horizontalScrolling: false,
+    verticalScrolling: true,
+    verticalOffset: 50,
+});
